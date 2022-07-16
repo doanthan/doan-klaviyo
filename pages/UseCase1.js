c3472010466661ac156f5741bab07724a36a41cd
Doan Than <doanthan@gmail.com>
2022-07-16 11:05:51 +1000

    fixed path

--- pages/UseCase1.js
+++ pages/UseCase1.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
-import { useState } from 'react';
-import Link from 'next/link';
-import { Form, Button, Container, Row, Col, Card } from 'reactstrap';
-import { useForm } from 'react-hook-form';
-import AddProfile from '../components/UseCase1/addProfile';
-import GetProfileDetails from '../components/UseCase1/GetProfileDetails';
+import axios from "axios";
+import { useState } from "react";
+import Link from "next/link";
+import { Form, Button, Container, Row, Col, Card } from "reactstrap";
+import { useForm } from "react-hook-form";
+import AddProfile from "../components/UseCase1/AddProfile";
+import GetProfileDetails from "../components/UseCase1/GetProfileDetails";
 
 const UseCase1 = () => {
   const { register, handleSubmit } = useForm();
@@ -48,18 +48,18 @@ const UseCase1 = () => {
   };
 
   return (
-    <Container className='px-4'>
-      <div className='text-center mt-40'>
+    <Container className="px-4">
+      <div className="text-center mt-40">
         <h1>Doan's Klaviyo Demo</h1>
-        <hr className='mt-40' />
-        <h5 className='text-secondary'>Use Case 1</h5>
-        <p className='px-5'>
+        <hr className="mt-40" />
+        <h5 className="text-secondary">Use Case 1</h5>
+        <p className="px-5">
           <b>Issue: </b>
           Brands may have messy address data from years of data collection. It
           is also hard to impress customers and show personalisation that gives
           a great customer experience.
         </p>
-        <p className='px-5'>
+        <p className="px-5">
           <b>Solution: </b>
           We call out to Google Maps to correctly fill out address fields ins
           Klaviyo. This helps with future location segmentation. We then use
@@ -76,20 +76,20 @@ const UseCase1 = () => {
         </i>
         <i>External APIs used in this example: Google Maps, OpenWeatherAPI</i>
       </div>
-      <Row className='justify-content-md-center'>
-        <Col sm='6'>
-          <Card className=' box-shadow '>
-            <div className='card-body'>
+      <Row className="justify-content-md-center">
+        <Col sm="6">
+          <Card className=" box-shadow ">
+            <div className="card-body">
               <Form onSubmit={handleSubmit(addPerson)}>
-                {' '}
+                {" "}
                 <AddProfile register={register} />
               </Form>
             </div>
           </Card>
         </Col>
-        <Col sm='6'>
-          <Card className=' box-shadow '>
-            <div className='card-body'>
+        <Col sm="6">
+          <Card className=" box-shadow ">
+            <div className="card-body">
               <p>
                 1. We post a new profile to Klaviyo. Note that the Address field
                 is in a single string- this makes it hard for marketers to
@@ -99,35 +99,35 @@ const UseCase1 = () => {
           </Card>
         </Col>
       </Row>
-      <Row className='justify-content-md-center pt-3'>
-        <Col sm='6'>
-          <Card className=' box-shadow '>
-            <div className='card-body'>
+      <Row className="justify-content-md-center pt-3">
+        <Col sm="6">
+          <Card className=" box-shadow ">
+            <div className="card-body">
               <Form onSubmit={handleSubmit2(getProfile)}>
-                {' '}
+                {" "}
                 <GetProfileDetails register={register2} />
               </Form>
             </div>
           </Card>
         </Col>
-        <Col sm='6'>
-          <Card className=' box-shadow '>
-            <div className='card-body'>
+        <Col sm="6">
+          <Card className=" box-shadow ">
+            <div className="card-body">
               <p>2. Retrieve the data:</p>
-              <pre>{JSON.stringify(profile, null, '\t')}</pre>
+              <pre>{JSON.stringify(profile, null, "\t")}</pre>
             </div>
           </Card>
         </Col>
       </Row>
-      <Row className='justify-content-md-center pt-3'>
-        <Col sm='6'>
-          <Card className=' box-shadow '>
-            <div className='card-body '>
-              <div className='pt-2'>
+      <Row className="justify-content-md-center pt-3">
+        <Col sm="6">
+          <Card className=" box-shadow ">
+            <div className="card-body ">
+              <div className="pt-2">
                 <Button
                   onClick={updateAddress}
-                  color='primary'
-                  className='btn-lg btn-block'
+                  color="primary"
+                  className="btn-lg btn-block"
                 >
                   Update
                 </Button>
@@ -135,9 +135,9 @@ const UseCase1 = () => {
             </div>
           </Card>
         </Col>
-        <Col sm='6'>
-          <Card className=' box-shadow '>
-            <div className='card-body'>
+        <Col sm="6">
+          <Card className=" box-shadow ">
+            <div className="card-body">
               3. In this step, we call out to Google API to search for the
               address that was entered in the first step. We then call back to
               Klaviyo to post the detailed location results of what was found
@@ -146,15 +146,15 @@ const UseCase1 = () => {
           </Card>
         </Col>
       </Row>
-      <Row className='justify-content-md-center pt-3'>
-        <Col sm='6'>
-          <Card className=' box-shadow '>
-            <div className='card-body'>
-              <div className='pt-2'>
+      <Row className="justify-content-md-center pt-3">
+        <Col sm="6">
+          <Card className=" box-shadow ">
+            <div className="card-body">
+              <div className="pt-2">
                 <Button
                   onClick={sendEmail}
-                  color='primary'
-                  className='btn-lg btn-block'
+                  color="primary"
+                  className="btn-lg btn-block"
                 >
                   Send
                 </Button>
@@ -162,9 +162,9 @@ const UseCase1 = () => {
             </div>
           </Card>
         </Col>
-        <Col sm='6'>
-          <Card className=' box-shadow '>
-            <div className='card-body'>
+        <Col sm="6">
+          <Card className=" box-shadow ">
+            <div className="card-body">
               4. Great! What else can we do with this information? In this
               example, we use the Longitude and Lattitude attributed found by
               Google to send a personal tailored message to the contact- an
@@ -173,10 +173,10 @@ const UseCase1 = () => {
           </Card>
         </Col>
       </Row>
-      <div className='text-center py-3'>
-        <Link href='/'>
+      <div className="text-center py-3">
+        <Link href="/">
           <a>
-            <Button type='submit' color='secondary' className='btn-lg '>
+            <Button type="submit" color="secondary" className="btn-lg ">
               Home
             </Button>
           </a>
