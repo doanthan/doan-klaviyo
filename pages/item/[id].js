d1c9e07a3c8407bdc084caa25c4f7a9794297148
Doan Than <doanthan@gmail.com>
2022-07-16 18:53:06 +1000

    fixing items

--- pages/item/[id].js
+++ pages/item/[id].js
@@ -14,7 +14,46 @@ const ItemPage = () => {
 
   const item = ITEM_LIST.find((item) => item.ProductID === id);
 
-  return <></>;
+  return (
+    <>
+      <Row>
+        <Col sm="3" className="pt-5 pl-5">
+          <Card className=" box-shadow">
+            <img
+              src={`/images/${item?.img}.jpeg`}
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <CardTitle className="card-title">
+                Buy this {item.ProductName}
+              </CardTitle>
+              <strong>{item.ProductName}</strong>
+              <p className="card-text mt-20">{item.Description}</p>
+            </div>
+            <Button
+              onClick={() => {
+                setVisible(!visible);
+              }}
+            >
+              Start Checkout
+            </Button>
+          </Card>
+        </Col>
+      </Row>
+
+      <Collapse horizontal isOpen={visible}>
+        <Alert
+          style={{
+            width: "500px",
+          }}
+        >
+          Added to Cart
+          {JSON.stringify(item, null, 2)}
+        </Alert>
+      </Collapse>
+    </>
+  );
 };
 
 export default ItemPage;
